f5d884833117d1c0d6aada3ae8f77c6979a94700
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-02-04 23:22:14 +0530

    Add links to frontend machine coding challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { Link } from 'react-router-dom';
+
 export default function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>Welcome to the app!</p>
+      <h1
+        style={{
+          marginBottom: '0.5rem',
+        }}
+      >
+        Frontend Machine Coding Challenges
+      </h1>
+      <p>
+        This is a collection of frontend machine coding challenges. These are
+        helpful for practicing and improving your frontend development skills.
+      </p>
+      <div>
+        <h4
+          style={{
+            marginTop: '2rem',
+          }}
+        >
+          Challenges
+        </h4>
+        <ul
+          style={{
+            padding: 20,
+            margin: 0,
+          }}
+        >
+          <li>
+            <Link
+              style={{
+                color: 'black',
+              }}
+              to="/todolist"
+            >
+              Todo List
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
